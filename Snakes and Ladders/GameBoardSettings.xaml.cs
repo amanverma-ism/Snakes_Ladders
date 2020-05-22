ef7465af0948c9c8b2da99292335b7c41fed2f14
Aman Verma <amanverma.ism@gmail.com>
2020-05-22 15:42:46 +0530

    updated

--- Snakes and Ladders/GameBoardSettings.xaml.cs
+++ Snakes and Ladders/GameBoardSettings.xaml.cs
@@ -277,7 +277,15 @@ namespace Snakes_and_Ladders
         {
             get
             {
-                return this.ActualHeight / 7.0;
+                return this.ActualHeight / 8.0;
+            }
+        }
+
+        public double RowPanelHeight
+        {
+            get
+            {
+                return RowHeight - 4.0;
             }
         }
 
@@ -293,7 +301,7 @@ namespace Snakes_and_Ladders
         {
             get
             {
-                return this.ActualHeight * 0.23;
+                return this.ActualHeight * 0.225;
             }
         }
 
@@ -609,61 +617,8 @@ namespace Snakes_and_Ladders
             OnPropertyChanged("ComboBoxHeight");
             OnPropertyChanged("ResetButtonWidth");
             OnPropertyChanged("CancelButtonWidth");
+            OnPropertyChanged("RowPanelHeight");
 
-        }
-
-        private DataTemplate getDataTemplate()
-        {
-            DataTemplate retVal = null;
-            String markup = String.Empty;
-
-            markup = "<DataTemplate xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" xmlns:local=\"clr-namespace:Snakes_and_Ladders;assembly=Snakes_and_Ladders\" DataType=	\"{x:Type SolidColorBrush}\">";
-            markup += "<Rectangle Width=\"";
-            markup += 20.ToString();
-            markup += "\" Height=\"{Binding RelativeSource={RelativeSource Mode=Self}, Path=Width}\" Fill=\"{Binding}\"/>";
-            markup += "</DataTemplate>";
-
-            retVal = (DataTemplate)XamlReader.Parse(markup);
-
-            return retVal;
-        }
-        
-
-        private DataTemplate GetColorDataTemplate()
-        {
-            return getDataTemplate();
-            ////create the data template
-            //DataTemplate colorboxtemplate = new DataTemplate();
-            //colorboxtemplate.DataType = ;
-
-            ////set up the stack panel
-            //FrameworkElementFactory spFactory = new FrameworkElementFactory(typeof(StackPanel));
-            //spFactory.Name = "myComboFactory";
-            //spFactory.SetValue(StackPanel.OrientationProperty, Orientation.Horizontal);
-
-            ////set up the card holder textblock
-            //FrameworkElementFactory cardHolder = new FrameworkElementFactory(typeof(TextBlock));
-            //cardHolder.SetBinding(TextBlock.TextProperty, new Binding("BillToName"));
-            //cardHolder.SetValue(TextBlock.ToolTipProperty, "Card Holder Name");
-            //spFactory.AppendChild(cardHolder);
-
-            ////set up the card number textblock
-            //FrameworkElementFactory cardNumber = new FrameworkElementFactory(typeof(TextBlock));
-            //cardNumber.SetBinding(TextBlock.TextProperty, new Binding("SafeNumber"));
-            //cardNumber.SetValue(TextBlock.ToolTipProperty, "Credit Card Number");
-            //spFactory.AppendChild(cardNumber);
-
-            ////set up the notes textblock
-            //FrameworkElementFactory notes = new FrameworkElementFactory(typeof(TextBlock));
-            //notes.SetBinding(TextBlock.TextProperty, new Binding("Notes"));
-            //notes.SetValue(TextBlock.ToolTipProperty, "Notes");
-            //spFactory.AppendChild(notes);
-
-            ////set the visual tree of the data template
-            //cardLayout.VisualTree = spFactory;
-
-            ////set the item template to be our shiny new data template
-            //drpCreditCardNumberWpf.ItemTemplate = cardLayout;
         }
     }
 }
